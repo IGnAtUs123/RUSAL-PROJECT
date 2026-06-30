d4e3bb0ab6febb146244ba160452cad66971978a
Ignat Kholodnyak <130066361+IGnAtUs123@users.noreply.github.com>
2026-06-30 17:38:59 +0700

    Implement email validation

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+document
+  .getElementById("incidentForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const emailInput = document.getElementById("email");
+    const emailValue = emailInput.value;
+
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regex.test(emailValue)) {
+      emailInput.style.borderColor = "red";
+
+      $("#dialog-text")
+        .text("Пожалуйста, введите корректный email.")
+        .css("font-size", "42px");
+
+      $("#dialog-message").dialog({
+        modal: true,
+        buttons: {
+          OK: function () {
+            $(this).dialog("close");
+          },
+        },
+      });
+    } else {
+      emailInput.style.borderColor = "green";
+
+      $("#dialog-text")
+        .text("Инцидент успешно отправлен.")
+        .css("font-size", "42px");
+
+      $("#dialog-message").dialog({
+        modal: true,
+        buttons: {
+          OK: function () {
+            $(this).dialog("close");
+          },
+        },
+      });
+    }
+  });
+
 const statusEl = document.getElementById("status");
 const statuses = document.querySelectorAll(".status");
 
